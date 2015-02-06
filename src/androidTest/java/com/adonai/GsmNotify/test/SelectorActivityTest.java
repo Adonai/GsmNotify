5f904e2bd99a9430516387c67e06abda8dfb7cd5
Адонай Элохим <adonai@xaker.ru>
2015-02-07 00:51:11 +0300

    add test for device add

---
+++ src/androidTest/java/com/adonai/GsmNotify/test/SelectorActivityTest.java
@@ -0,0 +1,59 @@
+package com.adonai.GsmNotify.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.UiThreadTest;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.adonai.GsmNotify.R;
+import com.adonai.GsmNotify.SettingsActivity;
+
+import java.util.Random;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class SelectorActivityTest extends ActivityInstrumentationTestCase2<SettingsActivity> {
+    Button mAddDeviceButton;
+    EditText mName, mNumber, mPassword;
+
+    public SelectorActivityTest() {
+        super(SettingsActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        mAddDeviceButton = (Button) getActivity().findViewById(R.id.device_apply_button);
+        mName = (EditText) getActivity().findViewById(R.id.device_name_text);
+        mNumber = (EditText) getActivity().findViewById(R.id.device_number_text);
+        mPassword = (EditText) getActivity().findViewById(R.id.device_password_text);
+    }
+
+    @UiThreadTest
+    public void testDeviceCreate() {
+        mName.setText(randomString());
+        mPassword.setText(randomPassword().toString());
+        mNumber.setText(randomNumber().toString());
+        mAddDeviceButton.performClick();
+    }
+
+    private String randomString() {
+        final Random rnd = new Random();
+        final String chars = "qwertyuiopasdfghjklzxcvbnm";
+        final int len = Math.abs(rnd.nextInt() % 10);
+        final StringBuilder sb = new StringBuilder(len);
+        for(int i = 0; i < len; ++i) {
+            sb.append(chars.charAt(Math.abs(rnd.nextInt() % chars.length())));
+        }
+        return sb.toString();
+    }
+
+    private Long randomNumber() {
+        return new Random().nextLong() % 99999999999l;
+    }
+
+    private Integer randomPassword() {
+        return new Random().nextInt() % 9999;
+    }
+}
